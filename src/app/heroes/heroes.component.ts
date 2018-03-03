4efc565efe165fc24198229ae0c867a8d7f93de2
dev00rob <phorton@softstackfactory.org>
2018-03-03 13:37:46 +0000

    add final touches to 4: Displaying a List

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,22 +9,22 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
   
-  selectedHero: Hero;
-  
-  onSelect(hero: Hero): void{
-    this.selectedHero = hero;
-  }
-  
   heroes = HEROES;
   
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  selectedHero: Hero;
+  
+  // hero: Hero = {
+  //   id: 1,
+  //   name: 'Windstorm'
+  // };
 
   constructor() { }
 
   ngOnInit() {
+  }
+  
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
   }
 
 }
